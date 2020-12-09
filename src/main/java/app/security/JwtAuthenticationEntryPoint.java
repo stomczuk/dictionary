6041df6f0fe6b5fa12c34aaf2b229224ce57686a
Sebastian Tomczuk <stomczuk.developer@gmail.com>
2020-12-09 10:38:12 +0100

    addJWTAuthenticationEntryPoint

---
+++ src/main/java/app/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,32 @@
+package app.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+public class JwtAuthenticationEntryPoint extends Http403ForbiddenEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+
+        ResponseEntity httpResponse =
+                new ResponseEntity(SecurityConstant.FORBIDDEN_MESSAGE,
+                        HttpStatus.FORBIDDEN);
+        response.setContentType(APPLICATION_JSON_VALUE);
+        response.setStatus(FORBIDDEN.value());
+        OutputStream outputStream = response.getOutputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(outputStream, httpResponse);
+        outputStream.flush();
+    }
+}
